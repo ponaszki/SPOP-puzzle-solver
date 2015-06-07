9aa76eed2137d670d93d6019e10579bbc0eb1c7d
ponaszki <ponaszkiewicz@gmail.com>
2015-06-07 21:49:15 +0200

    dodane wiersze z prawej strony

--- src/solverRow.hs
+++ src/solverRow.hs
@@ -1,6 +1,11 @@
+import Data.Matrix
 
-data Cell = Empty | Cell Int 
+data Cell = Cell Int --deriving Show 
 
+showCell :: Cell -> String
+showCell (Cell a) = show a
+instance Show Cell where
+	show(Cell c) = show c 
 --c :: Cell
 --c = 
 
@@ -8,8 +13,15 @@ data Cell = Empty | Cell Int
 --solveRow [] x = x
 --solveRow (i:is) (x:xs) | i>0 = 
 datarow = [[3], [2, 2], [2, 2], [3], [1], [1], [1], [1], [2], [3]]
-elem = [3]
-paintFromRight :: [Int] -> [Cell] -> [Cell]
+elem2 = 1:1:[]
+row = (Cell 0):(Cell 0):(Cell 0):(Cell 0):(Cell 0):[]
+paintFromRight :: [Integer] -> [Cell] -> [Cell]
 paintFromRight [] x = x
+paintFromRight [0] x = x
 paintFromRight (i:is) (c:cs) 	|i>0 = (Cell 1):paintFromRight ((i-1):is) cs 
 								|i==0 = (Cell 0): paintFromRight (is) cs
+
+
+paintfromLeft  ::[Integer] -> [Cell] -> [Cell]
+--paintfromLeft []
+paintFromLeft (i:is) (c:cs)	|i>0 = 
