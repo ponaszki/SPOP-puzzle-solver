3b8ea6a39721ff49f255b5ea906fe89a68fd3956
ponaszki <ponaszkiewicz@gmail.com>
2015-06-17 03:14:45 +0200

    dodane poprawki

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SPOP-puzzle-solver
 To repozytorium zawiera projekt z przedmiotu SPOP. Program napisany w Haskellu ma za zadanie rozwiązywać puzle polegające na zamalowywaniu plansz.
+Przed kompilacją należy pobrać modul Data.hmatrix. (cabal install data.matrix)
--- src/solver.hs
+++ src/solver.hs
@@ -6,13 +6,13 @@ import Debug.Trace
 
 main = do
 	putStrLn "Hello, Type in a path to file"  
-	let filename = "/home/ponaszki/workspace/haskell/SPOP-puzzle-solver/src/p1.txt"
-	--filename <- getLine  
+	let filename = "/home/ponaszki/workspace/haskell/SPOP-puzzle-solver/src/p2.txt"
+	filename <- getLine  
 	putStrLn ("Opening: " ++ filename )
 
 	--[s] <- getArgs --to do wczytywania pliku jako argument wywołania aplikacji
 	--putStrLn (s)
-	--handle <- openFile s ReadMode 
+	--handle <- openFile  ReadMode 
 	
 	handle <- openFile filename ReadMode
 	firstLine <- hGetLine handle
@@ -35,8 +35,7 @@ solvePuzzle rss css = solvePuzzle' rss css (matrix (length rss) (length css) $ \
 
 solvePuzzle' :: [[Int]] ->[[Int]] -> Matrix Int-> Matrix Int 
 solvePuzzle' rss css m 	|isAllMatrixFilled m  = m
---								|otherwise = checkAllRows rss 1 m
-								|otherwise = checkAllColumns css 1 (checkAllRows rss 1 m)
+								|otherwise = checkAllRows rss 1 (checkAllColumns css 1 m)
 
 -- funkcja sprawdzajaca czy cala matryca zostala wypelniona
 -- zwraca falsz jezeli nie i prawde jezeli tak	
@@ -60,8 +59,8 @@ checkIfOnlyOneOptionRow rowNr (id:ids) m 	|(sumList (id:ids))+((length (id:ids))
 getElementsToPaintFromLeft :: [Int]-> Int -> Int -> [Int]
 getElementsToPaintFromLeft [] _  _= []
 getElementsToPaintFromLeft [_] _ 0 = []
-getElementsToPaintFromLeft (id:ids) startpos len |id == 0 = [] ++ (getElementsToPaintFromLeft ids (startpos+1) (len-1))	 
-												|id >0 = [startpos] ++ (getElementsToPaintFromLeft ((id-1):ids) (startpos+1) (len-1))  
+getElementsToPaintFromLeft (id:ids) startpos len 	|id == 0 = [] ++ (getElementsToPaintFromLeft ids (startpos+1) (len-1))	 
+													|id >0 = [startpos] ++ (getElementsToPaintFromLeft ((id-1):ids) (startpos+1) (len-1))  
 
 -- funkcja zwraca liste indeksow elementow w danym wierszu ktore nalezy pomalowac idac od prawej
 getElementsToPaintFromRight :: [Int] -> Int -> [Int]
@@ -113,10 +112,15 @@ checkAllColumns (cs:css) colNr m = Debug.Trace.trace ("Calling cac with: css sn 
 checkIfOnlyOneOptionCol colNr (id:ids) m 	|(sumList (id:ids))+((length (id:ids))-1)==nrows m= True
 											| otherwise = False
 
---WYKRESLANIE 
---areGroupsFull Int -> [Int] ->Matrix Int -> Matrix Int
---areGroupsFull =  
 
+--WYKRESLANIE 
+
+--isRowFullfilled :: Int-> [Int] ->Matrix Int -> Bool
+--isRowFullfilled _ [] m = True
+--isRowFullfilled  rowNr (x:xs) m  	| sumList (x:xs) == length (getElementsToPaintInRow  x:xs (ncols m)) = True
+--							| otherwise = False
+
+--excludeFromFullRow :: Int ->  
 --getLengthOfBlock :: Int -> Int ->Matrix Int-> Int
 --getLengthOfBlock blockNr  rowNr m = 	if (isNotEmpty (getrow rowNr)) then
 
